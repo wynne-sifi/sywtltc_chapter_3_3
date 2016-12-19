538bf6bff947d8bf1d00a93ba03c702ba62314cf
John Graham <john.a.graham@gmail.com>
2016-12-19 22:28:14 +0000

    initial commit

---
+++ stats.py
@@ -0,0 +1,17 @@
+"""Some simple statistical functions."""
+
+def mean(data):
+    """Takes the mean/average of a list of numbers, a simple measure of 'middle'. See: https://en.wikipedia.org/wiki/Arithmetic_mean"""
+    pass
+
+def median(numbers):
+    """Finds the median or middle of a list of sorted numbers, a simple measure of 'middle'. See: https://en.wikipedia.org/wiki/Median"""
+    pass
+
+def range(numbers):
+    """Finds ths range of a list of numbers, a simple measure of variance. See: https://www.mathsisfun.com/definitions/range-statistics-.html"""
+    pass
+
+def standard_deviation(numbers):
+    """Finds the standard deviation of a list of numbers, a measure of variance. See: https://www.mathsisfun.com/data/standard-deviation-formulas.html"""
+    pass
